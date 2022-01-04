39be4050b29dc3858f41e5af8a64072ff6de2b36
Shamonti <67649277+Shamonti@users.noreply.github.com>
2022-01-04 10:23:51 +0600

    created user-profile page

--- frontend/script.js
+++ frontend/script.js
@@ -21,3 +21,20 @@ $(document).ready(function ($) {
     window.document.location = $(this).data('href');
   });
 });
+
+//SEE MORE BUTTON
+function readMore() {
+  let dots = document.getElementById('dots');
+  let moreText = document.getElementById('more');
+  let btnText = document.getElementById('seeMoreBtn');
+
+  if (dots.style.display === 'none') {
+    dots.style.display = 'inline';
+    btnText.innerHTML = 'See more';
+    moreText.style.display = 'none';
+  } else {
+    dots.style.display = 'none';
+    btnText.innerHTML = 'See less';
+    moreText.style.display = 'inline';
+  }
+}
